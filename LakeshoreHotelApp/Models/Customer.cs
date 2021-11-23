f52c3675f03d1421a49c794a0d8d9a1af33829d0
ndforler <ndforler@lakeheadu.ca>
2021-11-23 14:27:08 -0500

    added models

---
+++ LakeshoreHotelApp/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LakeshoreHotelApp.Models
+{
+    public class Customer
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int PhoneNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Address { get; set; }
+        //parent object refference
+        public List<Room> Rooms { get; set; }
+    }
+}
