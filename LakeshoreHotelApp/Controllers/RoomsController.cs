7ea88e0263d3a8c53db7de9934f1e643fae00711
ndforler <ndforler@lakeheadu.ca>
2021-11-25 02:11:49 -0500

    working on the employee aspect of the project

---
+++ LakeshoreHotelApp/Controllers/RoomsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LakeshoreHotelApp.Data;
+using LakeshoreHotelApp.Models;
+using System.Security.Claims;
+
+namespace LakeshoreHotelApp.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoomsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rooms
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.rooms.ToListAsync());
+        }
+
+        // GET: Rooms/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.rooms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // GET: Rooms/Create
+        public IActionResult Create()
+        {
+            ViewBag.customer = GetCustomerById(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            ViewBag.rooms = _context.Set<Room>().ToList();
+            return View("Create");
+        }
+
+        public Customer GetCustomerById(string accountId)
+        {
+            // compare accountId to the Id of each existing customer, return the first one that matches, or null if none match
+            foreach (Customer cus in _context.Set<Customer>())
+            {
+                if (cus.Id.Equals(accountId)) return cus;
+            }
+            return null;
+        }
+
+        // POST: Rooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RoomNumber,RoomFilled,BedSize,RoomType,ReservationStart,ReservationEnd")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(room);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.rooms.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Rooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,RoomNumber,RoomFilled,BedSize,RoomType,ReservationStart,ReservationEnd")] Room room)
+        {
+            if (id != room.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.rooms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var room = await _context.rooms.FindAsync(id);
+            _context.rooms.Remove(room.Customer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomExists(string id)
+        {
+            return _context.rooms.Any(e => e.Id == id);
+        }
+    }
+}
