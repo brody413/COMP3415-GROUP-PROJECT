8da8dc8e939b7a6e64690324146e435433342e90
Parth Joshi <pjoshi4@lakeheadu.ca>
2021-11-25 13:51:32 -0500

    styling

--- LakeshoreHotelApp/wwwroot/js/site.js
+++ LakeshoreHotelApp/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let map;
+
+/* Source: Google Maps API Overview 
+ * https://developers.google.com/maps/documentation/javascript/markers#maps_marker_simple-javascript
+ * https://developers.google.com/maps/documentation/javascript/overview?csw=1#maps_map_simple-html
+ */ 
+
+function initMap() {
+    const loc = { lat: 44.38963, lng: -79.68212 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: loc,
+        zoom: 18
+    });
+
+    new google.maps.Marker({
+        position: loc,
+        map,
+        title: "Lakeshore Hotel",
+    });
+}
