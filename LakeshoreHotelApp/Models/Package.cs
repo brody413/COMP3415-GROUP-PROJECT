4b3ef0e7a132683a764f86ea458fce9e4ad99f05
Parth Joshi <pjoshi4@lakeheadu.ca>
2021-12-10 12:29:13 -0500

    added package deals

---
+++ LakeshoreHotelApp/Models/Package.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LakeshoreHotelApp.Models
+{
+    public class Package
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set;  }
+
+        [Required]
+        public int TypeId { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        [Range(0.01, 99999)]
+        public double Price { get; set; }
+
+        [Required]
+        public string Duration { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string Picture { get; set; }
+
+    }
+}
